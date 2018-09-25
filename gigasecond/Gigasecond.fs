80be3429cb4f5882c91584fdcbd763725619b9ff
Xemi Sánchez <xemisan@gmail.com>
2018-09-25 09:53:14 +0200

    Commit all exercises done so far

---
+++ gigasecond/Gigasecond.fs
@@ -0,0 +1,5 @@
+﻿module Gigasecond
+open System
+
+let add (beginDate: DateTime): DateTime = 
+    beginDate.AddSeconds(float(pown 10 9))
---
+++ leap/Leap.fs
@@ -0,0 +1,8 @@
+﻿module Leap
+
+let leapYear (year: int): bool = 
+    if year % 4 <> 0 then 
+        false
+    else if year % 100 <> 0 then 
+            true
+        else year % 400 = 0
