f560e7eddd2eb7c6331ec38a0454a87afe71e28b
Sánchez <Jose.Miguel.Sanchez@dnvgl.com>
2018-09-25 08:42:28 +0200

    Commit all exercises done so far

---
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,11 @@
+﻿module DifferenceOfSquares
+
+let squareOfSum (number: int): int = 
+    let sum = List.sum [1 .. number]
+    sum*sum
+
+let sumOfSquares (number: int): int = 
+    List.sumBy (fun elem -> elem*elem) [1 .. number]
+
+let differenceOfSquares (number: int): int = 
+    squareOfSum number - sumOfSquares number
---
+++ reverse-string/ReverseString.fs
@@ -0,0 +1,4 @@
+﻿module ReverseString
+
+let reverse (input: string): string =
+    System.String.Concat([| for i in input.Length-1 .. -1 .. 0 -> input.[i] |])
