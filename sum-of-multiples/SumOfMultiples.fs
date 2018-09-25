8e1b51ddc8e0ddcaa5c0929e5d2352f76d5b551d
Xemi Sánchez <xemisan@gmail.com>
2018-09-25 18:30:42 +0200

    Commit 'Sum Of Multiples' exercise

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,3 +1,14 @@
 ﻿module SumOfMultiples
 
-let sum (numbers: int list) (upperBound: int): int = failwith "You need to implement this function."
+let keepNumberIfDivisible number divisor  = 
+    match number % divisor = 0 with
+        | true -> number
+        |_ -> 0
+
+let cleanNumbersThatDontHaveDivisors (divisors: int list) (number: int): int =
+    let list = divisors |> List.map (keepNumberIfDivisible number) |> List.toArray |> Array.filter ((<>)0)
+    if list.Length > 0 then list.[0]
+    else 0
+
+let sum (numbers: int list) (upperBound: int): int =
+    [1..upperBound-1] |> List.map (cleanNumbersThatDontHaveDivisors numbers) |> List.sum
