e57bce1d22de170fa5c2f6b27a6918438e7fd0fe
Xemi Sánchez <xemisan@gmail.com>
2018-09-26 10:37:58 +0200

    First code review

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,14 +1,13 @@
 ﻿module SumOfMultiples
 
 let keepNumberIfDivisible number divisor  = 
-    match number % divisor = 0 with
-        | true -> number
-        |_ -> 0
-
-let cleanNumbersThatDontHaveDivisors (divisors: int list) (number: int): int =
-    let list = divisors |> List.map (keepNumberIfDivisible number) |> List.toArray |> Array.filter ((<>)0)
-    if list.Length > 0 then list.[0]
+    if number % divisor = 0 then number
     else 0
 
+let cleanNumbersThatDontHaveDivisors (divisors: int list) (number: int): int =
+    let list = divisors |> List.map (keepNumberIfDivisible number) |> List.filter ((<>)0)
+    if list.Length > 0 then list.[0]
+    else 0
+    
 let sum (numbers: int list) (upperBound: int): int =
     [1..upperBound-1] |> List.map (cleanNumbersThatDontHaveDivisors numbers) |> List.sum
