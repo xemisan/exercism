04cc4015f88ea12c82d40aba89fd445069c58f1d
Xemi Sánchez <xemisan@gmail.com>
2018-10-02 12:53:42 +0200

    Small changes after mentor review.

--- space-age/SpaceAge.fs
+++ space-age/SpaceAge.fs
@@ -6,13 +6,13 @@ let earthYearsToSeconds orbitalPeriod = orbitalPeriod * 365.25 * 24.0 * 60.0 * 6
 
 let orbitalPeriod planet = 
     match planet with
-        | Earth -> earthYearsToSeconds 1.0
-        | Mercury -> earthYearsToSeconds 0.2408467
-        | Venus -> earthYearsToSeconds 0.61519726
-        | Mars -> earthYearsToSeconds 1.8808158
-        | Jupiter -> earthYearsToSeconds 11.862615
-        | Saturn -> earthYearsToSeconds 29.447498
-        | Uranus -> earthYearsToSeconds 84.016846
-        | Neptune -> earthYearsToSeconds 164.79132
+    | Earth -> earthYearsToSeconds 1.0
+    | Mercury -> earthYearsToSeconds 0.2408467
+    | Venus -> earthYearsToSeconds 0.61519726
+    | Mars -> earthYearsToSeconds 1.8808158
+    | Jupiter -> earthYearsToSeconds 11.862615
+    | Saturn -> earthYearsToSeconds 29.447498
+    | Uranus -> earthYearsToSeconds 84.016846
+    | Neptune -> earthYearsToSeconds 164.79132
 
-let age (planet: Planet) (seconds: int64): float = (float seconds) / orbitalPeriod planet
+let age (planet: Planet) (seconds: int64): float = float seconds / orbitalPeriod planet
