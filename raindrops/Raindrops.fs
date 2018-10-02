777ec336ce6272ccfede62c176cbae0b41ca6763
Xemi Sánchez <xemisan@gmail.com>
2018-10-02 17:17:11 +0200

    Commit raindrops exercise

---
+++ raindrops/Raindrops.fs
@@ -0,0 +1,12 @@
+﻿module Raindrops
+
+let sounds = dict[3, "Pling"; 5, "Plang"; 7, "Plong"]
+
+let addDefault number input = if input = System.String.Empty then number.ToString() else input
+
+let convert (number: int): string = 
+    sounds.Keys |> 
+        Seq.filter(fun f -> number % f = 0) |>
+            Seq.map (fun f -> sounds.Item(f)) |> 
+                System.String.Concat |> 
+                    addDefault number
