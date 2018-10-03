66c6a6a7ff1fb36e8ffbdd4e2144ace64dedeb4b
Xemi Sánchez <xemisan@gmail.com>
2018-10-03 10:41:05 +0200

    Submit the first version of the hamming exercise.

---
+++ hamming/Hamming.fs
@@ -0,0 +1,9 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option = 
+    if strand1.Length <> strand2.Length then None
+    else
+        let count = [| for i in 0 .. strand1.Length-1 -> strand1.[i] = strand2.[i] |] |> 
+                        Array.filter (fun x -> x = false) |> 
+                            Array.length
+        Some count
