962c9d8945451868a64207a3899f433e34899f91
Xemi Sánchez <xemisan@gmail.com>
2018-10-04 08:50:23 +0200

    Second iteration of the accumulate exercise after the mentor's review.

--- accumulate/Accumulate.fs
+++ accumulate/Accumulate.fs
@@ -1,6 +1,6 @@
 ﻿module Accumulate
 
 let rec accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
-    // input |> List.map func
-    if input.IsEmpty then List.Empty
-    else accumulate func input.Tail |> List.append [func input.[0]]
+    match input with
+    | [] -> List.empty
+    | _ -> func input.[0]::accumulate func input.Tail
