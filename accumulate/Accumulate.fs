ee039665a6138d23bfc4275cc62e9bdf45fe025c
Xemi Sánchez <xemisan@gmail.com>
2018-10-04 13:40:40 +0200

    Final iteration of the accumulate exercise.

--- accumulate/Accumulate.fs
+++ accumulate/Accumulate.fs
@@ -1,6 +1,14 @@
 ﻿module Accumulate
 
-let rec accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
-    match input with
-    | [] -> List.empty
-    | head::tail -> func head::accumulate func tail
+let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
+    // let rec loop func input accumulator = 
+    //     match input with
+    //     | [] -> List.rev accumulator
+    //     | head::tail -> loop func tail (func head::accumulator)
+    // loop func input []
+
+    let rec loop accumulator = function
+        | [] -> List.rev accumulator
+        | head::tail -> loop (func head::accumulator) tail 
+        
+    loop [] input
