f3942dcc9f683d37d176f7f1b5449881dd56a763
Xemi Sánchez <xemisan@gmail.com>
2018-10-05 13:15:33 +0200

    Commit first version of the grade school exercise.

---
+++ grade-school/GradeSchool.fs
@@ -0,0 +1,20 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School): School = 
+    match school.ContainsKey(grade) with
+    | true -> school.Add(grade, student::school.[grade])
+    | _ -> school.Add(grade, [student])
+
+let roster (school: School): (int * string list) list = 
+    school
+    |> Map.map (fun _ value -> List.sort value)
+    |> Map.toList
+
+let grade (number: int) (school: School): string list =
+    match school.ContainsKey(number) with
+    | true -> List.sort school.[number]
+    | _ -> []
