acac0ae79d28d396d858880fe0eddd2b6fd6e29d
Xemi Sánchez <xemisan@gmail.com>
2018-10-05 13:32:48 +0200

    Commit solution to the pangram exercise.

---
+++ pangram/Pangram.fs
@@ -0,0 +1,7 @@
+﻿module Pangram
+
+open System.Text.RegularExpressions
+
+let pattern = "(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z).*"
+
+let isPangram (input: string): bool = Regex.Match(input.ToLower(), pattern).Success
