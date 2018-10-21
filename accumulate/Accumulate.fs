b9c3c93520e52d91905a1160e8b1e6797c2860f3
Xemi Sánchez <xemisan@gmail.com>
2018-10-21 21:38:34 +0200

    Remove unused code from accumulate exercise.

--- accumulate/Accumulate.fs
+++ accumulate/Accumulate.fs
@@ -1,12 +1,6 @@
 ﻿module Accumulate
 
 let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
-    // let rec loop func input accumulator = 
-    //     match input with
-    //     | [] -> List.rev accumulator
-    //     | head::tail -> loop func tail (func head::accumulator)
-    // loop func input []
-
     let rec loop accumulator = function
         | [] -> List.rev accumulator
         | head::tail -> loop (func head::accumulator) tail 
