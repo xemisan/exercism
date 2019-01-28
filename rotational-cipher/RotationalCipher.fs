117de6acd4b3c67d4f907499b75d539100feaf4d
Xemi Sánchez <xemisan@gmail.com>
2019-01-28 10:51:16 +0100

    Refactor rotational cipher solution.

--- rotational-cipher/RotationalCipher.fs
+++ rotational-cipher/RotationalCipher.fs
@@ -1,22 +1,16 @@
 ﻿module RotationalCipher
 
-let isUpperCase number =
-    number >= 65 && number <= 90
-    
-let isLowerCase number =
-    number >= 97 && number <= 122
+open System
 
-let convert (k: int) (c:char): char =
-    let value = (int c)
-    let newValue = value + k
-
-    match (isUpperCase value, newValue > 90, isLowerCase value, newValue > 122) with
-    | false, _, false, _ -> c
-    | true, true, _, _ -> char (newValue - 26)
-    | _, _, true, true -> char (newValue - 26)
-    | _, _, _, _ -> char newValue
+let rotateLetter (k: int) (c:char): char =
+    let value = (int c) + k
+    match (Char.IsLetter c, Char.IsUpper c, Char.IsLower c, value > 90, value > 122) with
+    | false, _, _, _, _ -> c
+    | _, true, _, true, _ -> char (value - 26)
+    | _, _, true, _, true -> char (value - 26)
+    | _, _, _, _, _ -> char value
 
 let rotate (shiftKey: int) (text: string) =
     text
-    |> Seq.map (fun c -> convert shiftKey c )
-    |> System.String.Concat
+    |> Seq.map (rotateLetter shiftKey)
+    |> String.Concat
