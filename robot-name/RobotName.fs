8e73a2059ceb6f9c4987cb022a9a27adf385b87e
Xemi Sánchez <xemisan@gmail.com>
2019-02-24 22:16:07 +0100

    Commit the solution for the 'Robot name' exercise.

---
+++ robot-name/RobotName.fs
@@ -0,0 +1,21 @@
+﻿module RobotName
+
+type Robot = { Name: string }
+
+let randomName() = 
+    let rnd = System.Random()
+    System.String.Concat [
+        (rnd.Next(65, 90) |> char).ToString(); 
+        (rnd.Next(65, 90) |> char).ToString(); 
+        rnd.Next(0, 9).ToString();
+        rnd.Next(0, 9).ToString(); 
+        rnd.Next(0, 9).ToString()]
+
+let rec mkRobot() = 
+    { Name = randomName() }
+    
+let name (robot: Robot): string = 
+    robot.Name
+
+let reset robot = 
+    { robot with Name = randomName() }
