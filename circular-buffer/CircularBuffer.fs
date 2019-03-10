d7f83abd9645a245128db919f74394e56c073f2c
Xemi Sánchez <xemisan@gmail.com>
2019-03-10 21:05:23 +0100

    Refactor circular buffer after mentor review

--- circular-buffer/CircularBuffer.fs
+++ circular-buffer/CircularBuffer.fs
@@ -4,17 +4,17 @@ type Buffer = {size: int; elements: int list}
 
 let mkCircularBuffer x = { size = x; elements = []}
 
-let clear buffer = { size = buffer.size; elements = []}
+let clear buffer = { buffer with elements = [] }
 
 let write value buffer =
     match buffer.elements.Length = buffer.size with
     | true -> System.Exception() |> raise
-    | _ -> { size = buffer.size; elements = value::(buffer.elements |> List.rev) |> List.rev  }
+    | _ -> { buffer with elements = buffer.elements @ [value] }
 
 let read buffer = 
     match buffer.elements with
     | [] -> System.Exception() |> raise
-    | head::tail -> (head, { size = buffer.size; elements = tail })
+    | head::tail -> (head, { buffer with elements = tail })
 
 let forceWrite value buffer =
     match buffer.elements.Length = buffer.size with
